26f308f2d2fa2e936e523edfb32a6689a53470d2
benjamin batundi <benbatundi7@gmail.com>
2024-01-08 18:05:28 +0100

    finishing starting pages

--- lib/app/start/shared/utils/route.dart
+++ lib/app/start/shared/utils/route.dart
@@ -1,6 +1,8 @@
 import 'package:flow_projet/app/start/views/start/anounce_page.dart';
+import 'package:flow_projet/app/start/views/start/start_page.dart';
 import 'package:flutter/material.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   AnouncePage.routeName: (context) => const AnouncePage(),
+  StartPage.routeName: (context) => const StartPage()
 };
